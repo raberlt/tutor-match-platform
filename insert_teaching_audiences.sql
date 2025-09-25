39b977b0a737509556f0357bfabbabbf0b079eb8
PhatNguyen24 <daiphat2442003@gmail.com>
2025-09-25 16:30:34 +0700

    remove file not important

--- insert_teaching_audiences.sql
+++
@@ -1,9 +0,0 @@
--- Insert teaching audiences data
-INSERT INTO teaching_audiences (name, created_at, updated_at) VALUES
-('Học tự do', GETDATE(), GETDATE()),
-('Trung học cơ sở', GETDATE(), GETDATE()),
-('Trung học phổ thông', GETDATE(), GETDATE()),
-('Trung cấp nghề', GETDATE(), GETDATE()),
-('Cao đẳng / Đại học', GETDATE(), GETDATE()),
-('Sau đại học', GETDATE(), GETDATE()),
-('Người đi làm', GETDATE(), GETDATE());
